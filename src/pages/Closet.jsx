016b42febdc1b216e78ee85cb80b91bfcd1b5016
Sonoe <sonoea@gmail.com>
2026-02-10 14:11:58 +0900

    Fix: Remove MOCK_GALLERY import to fix potential TDZ issue

--- src/pages/Closet.jsx
+++ src/pages/Closet.jsx
@@ -8,9 +8,27 @@ import { Edit2, Trash2, Plus, Shirt, Users, Heart, Share2, Lock, Unlock, X, Came
 import { compressImage } from '../utils/imageUtils';
 // import AddItemModal from '../components/AddItemModal';
 import Portal from '../components/Portal';
-import { MOCK_GALLERY } from '../constants/mockData';
+// import { MOCK_GALLERY } from '../constants/mockData';
 
 const AddItemModal = lazy(() => import('../components/AddItemModal'));
+
+const MOCK_GALLERY = [
+  {
+    id: 'g0',
+    userName: 'うなえさん',
+    userIcon: '/unae-san.png',
+    plushieName: 'うなえさん',
+    plushieHeight: 12,
+    location: '東京',
+    imageUrl: '/sample-outfit.jpg',
+    itemName: 'パンダのドレス',
+    shopName: 'ダイソー',
+    fitRating: 2,
+    comment: 'ダイソーの椅子の靴下です。ヒレを通す穴を開ければ、うなえさんにぴったりのドレスに変身！',
+    date: '2026-02-07',
+    likes: 42,
+  }
+];
 
 // --- MAIN CLOSET COMPONENT ---
 const Closet = () => {
